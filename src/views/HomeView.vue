2a0f800bb1d1297968ee349b3ffb26fa8da9076f
cellichm <103275696+cellichm@users.noreply.github.com>
2023-09-17 22:12:34 +0000

    Adding reservations home view and cancelling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,62 @@
 <script setup>
-const reservations = [
-	{
-		id: 1,
-		location: "pula-city-mall",
-		user: "mcellich",
-		date: "2023-09-10",
-		timeStart: "11:00",
-		timeEnd: "11:50",
-		washProgram: 1
-	},
-];
+import { ref } from 'vue';
+import axios from 'axios';
+
+// Environment variable to check if the URL should be from local or from real API.
+const baseUrl = import.meta.env.DEV ? 'http://127.0.0.1:3000' : '';
+
+const fetchDataPoint = async (slug) => {
+	let items = [];
+
+	try {
+		const { data } = await axios.get(`${baseUrl}/${slug}`);
+
+		if (data.status === 'ok') {
+			items = data.data;
+		} else {
+			console.error('Error while fetching locations');
+
+			return null;
+		}
+
+	} catch (error) {
+		console.error(error);
+		return null;
+	}
+
+	return items;
+};
+
+const reservations = ref([]);
+const washPrograms = ref([]);
+const washSteps = ref([]);
+const locations = ref([]);
+
+const getNeededData = async () => {
+	reservations.value = await fetchDataPoint('reservations');
+	washPrograms.value = await fetchDataPoint('wash-programs');
+	washSteps.value = await fetchDataPoint('wash-steps');
+	locations.value = await fetchDataPoint('locations');
+};
+
+getNeededData();
+
+const deleteReservation = async (id) => {
+	try {
+		const response = await axios.delete(`${baseUrl}/reservation/${id}`);
+
+		if (response.data.status === 'ok') {
+			alert('Uspješno ste otkazali termin!');
+
+			reservations.value = reservations.value.filter((reservation) => reservation._id !== id);
+		} else {
+			alert('Došlo je do greške prilikom otkazivanja termina.');
+		}
+	} catch (error) {
+		console.error(error);
+		alert('Došlo je do greške prilikom otkazivanja termina.');
+	}
+}
 </script>
 
 <template>
@@ -18,9 +65,14 @@ const reservations = [
 	<h2 class="mb-3">Rezervacije</h2>
 	<div class="d-flex flex-wrap gap-2 mb-5">
 		<div class="card" v-for="reservation in reservations" :key="reservation.id" style="width: 24rem">
-			<div class="card-header">{{ reservation.date }} {{ reservation.timeStart }} - {{ reservation.timeEnd }}</div>
+			<div class="card-header">{{ new Date(reservation.date).toLocaleDateString() }} <br /> {{ reservation.timeStart
+			}} - {{ reservation.timeEnd }} <br /> {{ locations.find((loc) => loc.id === reservation.location)?.name }}
+			</div>
 			<div class="card-body">
-				<div class="card-text">{{ reservation.washProgram }}</div>
+				<div class="card-text">{{ reservation.washProgram === '-1' ? 'Prilagođeno pranje' : washPrograms.find((prog) => prog.id === reservation.washProgram)?.name }}</div>
+			</div>
+			<div class="card-footer">
+				<button class="btn btn-sm btn-danger" @click="deleteReservation(reservation._id)">Otkaži</button>
 			</div>
 		</div>
 	</div>
