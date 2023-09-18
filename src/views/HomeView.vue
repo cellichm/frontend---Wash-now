8e0fbee36da9c7f10e7025658b5a8952ba0f2e61
cellichm <103275696+cellichm@users.noreply.github.com>
2023-09-18 00:17:55 +0000

    Fixing API URL

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { RouterLink } from 'vue-router';
 
 // Environment variable to check if the URL should be from local or from real API.
-const baseUrl = import.meta.env.DEV ? 'http://127.0.0.1:3000' : (import.meta.env.API_URL ?? 'https://mcellich-wash-now-backend.onrender.com/');
+const baseUrl = import.meta.env.DEV ? 'http://127.0.0.1:3000' : (import.meta.env.API_URL ?? 'https://mcellich-wash-now-backend.onrender.com');
 
 const userToken = localStorage.getItem('washNowUserToken');
 const username = localStorage.getItem('washNowUserUsername');
